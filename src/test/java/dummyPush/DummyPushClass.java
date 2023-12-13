a32ff92a00fc15b3149fac19963633db2c0c5547
saisu <saisu@DESKTOP-LJMQIG8>
2023-12-13 22:04:12 +0530

    Updated code

--- src/test/java/dummyPush/DummyPushClass.java
+++ src/test/java/dummyPush/DummyPushClass.java
@@ -3,5 +3,6 @@ package dummyPush;
 public class DummyPushClass {
 	public static void main(String[] args) {
 		System.out.println("This is my forst commit..........");
+		System.out.println("I am doing the changes");
 	}
 }
