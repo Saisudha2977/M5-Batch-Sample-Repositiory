eb38668cfee0956bf818c0884911b423b9d0fc5b
saisu <saisu@DESKTOP-LJMQIG8>
2023-12-14 20:08:00 +0530

    TC_002 committed

--- src/test/java/dummyPush/TC_001.java
+++ src/test/java/dummyPush/TC_001.java
@@ -3,5 +3,9 @@ package dummyPush;
 public class TC_001 {
 	public static void main(String[] args) {
 		System.out.println("TC_001 testcase code has been written");
+		
+		//TC_002
+		System.out.println("HI..............");
 	}
+	
 }
